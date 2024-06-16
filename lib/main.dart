5e67380bc4c855414a5740aa41dd7bca5813669d
Pedro-Tuto <pedropb97@gmail.com>
2024-06-16 20:52:45 -0300

    Add Post User Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petshop/pages/user_create_page.dart';
 import 'package:petshop/pages/user_info_page.dart';
 
 void main() {
@@ -17,7 +18,10 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Petshop App'),
-      routes: {"/user-info": (context)=> const UserInfoPage()},
+      routes: {
+        "/user-info": (context) => const UserInfoPage(),
+        "/user-create": (context) => const UserCreatePage(),
+      },
     );
   }
 }
@@ -32,11 +36,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -51,6 +52,15 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.of(context).pushNamed("/user-info");
               },
               child: Text("Usuários"),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/user-create");
+                },
+                child: Text("Cadastrar Usuário"),
+              ),
             ),
           ],
         ),
