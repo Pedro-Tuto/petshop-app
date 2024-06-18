f35731291a94d4b9e16c8b985b927db1dd24a0ff
JeffCarradore <jeffersoncarradore001@gmail.com>
2024-06-18 16:21:06 -0300

    Header com botões funcionais

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
       title: 'Petshop App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+
       home: const MyHomePage(title: 'Petshop App'),
+      
       routes: {
         "/user-info": (context) => const UserInfoPage(),
         "/user-create": (context) => const UserCreatePage(),
@@ -45,52 +45,93 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed("/user-info");
-              },
-              child: Text("Usuários"),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed("/user-create");
-                },
-                child: Text("Cadastrar Usuário"),
+
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+
+      //ListView
+      body: ListView(
+        scrollDirection: Axis.vertical,
+
+        //Divisões
+        children: [
+
+          //MAIN
+            Container(
+              height: 60,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              width: double.maxFinite,
+
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(colors: [Color.fromARGB(0, 0, 217, 255), Color.fromARGB(255, 115, 190, 233),],),
+                borderRadius: BorderRadius.circular(100),
+              ),
+
+              //Header
+              child: Row(
+                children: [
+
+                  //Logo
+                  const Padding(padding: EdgeInsets.only(left: 20),
+                    child: Text("Petshop App", style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 15, 52, 61),
+                  ),),
+                  ),
+
+                  
+                  
+                  Spacer(),
+
+                  //Botões
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Row(
+                      children: [
+                        //Botão User
+                        TextButton(onPressed: (){Navigator.of(context).pushNamed("/user-create");}, child: const Text("New User", style: TextStyle(color: Colors.white),),),
+
+                        //Botão Pet
+                        TextButton(onPressed: (){Navigator.of(context).pushNamed("/pet-create");}, child: const Text("New Pet", style: TextStyle(color: Colors.white),),),
+                      ],
+                    ),
+                  ),
+
+
+                  
+                ],
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed("/user-delete");
-              },
-              child: Text("Deletar Usuário"),
+
+          //BANNER
+            Container(
+              height: 500,
+              width: double.maxFinite,
             ),
-            Padding(
-                padding: EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed("/pet-info");
-                  },
-                  child: Text("Pets"),
-                )),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed("/pet-create");
-              },
-              child: Text("Cadastrar Pet"),
+
+          //USER
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.black87,
             ),
-          ],
-        ),
+
+          //USER
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+
+
+        ],
       ),
-    );
+
+
+      
+
+
+
+
+      );
   }
 }
