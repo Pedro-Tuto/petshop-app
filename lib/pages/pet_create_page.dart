f101627b955617c504475e3a2df051352b2a048d
Pedro-Tuto <pedropb97@gmail.com>
2024-06-17 23:06:15 -0300

    Add Form resets, confirmation pop-ups

--- lib/pages/pet_create_page.dart
+++ lib/pages/pet_create_page.dart
@@ -6,168 +6,224 @@ class PetCreatePage extends StatefulWidget {
   const PetCreatePage({super.key});
 
   @override
-  State<PetCreatePage> createState() => _UserInfoPageState();
+  State<PetCreatePage> createState() => _PetCreatePageState();
 }
 
-class _UserInfoPageState extends State<PetCreatePage> {
+class _PetCreatePageState extends State<PetCreatePage> {
   final form = <String, dynamic>{};
+
+  // Controllers para os campos de texto
+  final nameController = TextEditingController();
+  final breedController = TextEditingController();
+  final colorController = TextEditingController();
+  final dateOfBirthController = TextEditingController();
+  final weightController = TextEditingController();
+  final ownerIdController = TextEditingController();
 
   void createPet() async {
     final petCreate = PetCreate.fromJson(form);
     await api.postPetPetsPost(petCreate!);
+
+    // Limpar os campos após a criação do pet
+    nameController.clear();
+    breedController.clear();
+    colorController.clear();
+    dateOfBirthController.clear();
+    weightController.clear();
+    ownerIdController.clear();
+
+    // Resetar o formulário
+    form.clear();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Sucesso"),
+          content: Text("Pet cadastrado com sucesso!"),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar o diálogo
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Cadastrar Pet"),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        body: Container(
-          alignment: Alignment.topCenter,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: SizedBox(
-                      width: 300.0,
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        maxLines: 1,
-                        style: const TextStyle(
-                          fontSize: 13,
-                        ),
-                        decoration: const InputDecoration(
-                          labelText: "Nome",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          form["name"] = value;
-                        },
-                      ))),
-              SizedBox(
+      appBar: AppBar(
+        title: Text("Cadastrar Pet"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
                 width: 300.0,
                 child: TextField(
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    style: const TextStyle(
-                      fontSize: 13,
-                    ),
-                    decoration: const InputDecoration(
-                      labelText: "Raça",
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                  controller: nameController,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Nome",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
                       ),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
                     ),
-                    onChanged: (value) {
-                      form["breed"] = value;
-                    }),
+                  ),
+                  onChanged: (value) {
+                    form["name"] = value;
+                  },
+                ),
               ),
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: SizedBox(
-                      width: 300.0,
-                      child: TextField(
-                          textAlign: TextAlign.center,
-                          maxLines: 1,
-                          style: const TextStyle(
-                            fontSize: 13,
-                          ),
-                          decoration: const InputDecoration(
-                            labelText: "Cor",
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.black,
-                              ),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                            ),
-                          ),
-                          onChanged: (value) {
-                            form["color"] = value;
-                          }))),
-              SizedBox(
-                  width: 300.0,
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    style: const TextStyle(
-                      fontSize: 13,
+            ),
+            SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: breedController,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontSize: 13,
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Raça",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
                     ),
-                    decoration: const InputDecoration(
-                      labelText: "Data de nascimento",
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                onChanged: (value) {
+                  form["breed"] = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
+                width: 300.0,
+                child: TextField(
+                  controller: colorController,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Cor",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
                       ),
-                      hintText: "yyyy-MM-dd",
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
                     ),
-                    onChanged: (value) {
-                      form["date_of_birth"] = value;
-                    },
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: SizedBox(
-                      width: 300.0,
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        maxLines: 1,
-                        style: const TextStyle(
-                          fontSize: 13,
-                        ),
-                        decoration: const InputDecoration(
-                          labelText: "Peso",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          form["weight"] = value;
-                        },
-                      ))),
-              SizedBox(
-                  width: 300.0,
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    style: const TextStyle(
-                      fontSize: 13,
+                  ),
+                  onChanged: (value) {
+                    form["color"] = value;
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: dateOfBirthController,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontSize: 13,
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Data de nascimento",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
                     ),
-                    decoration: const InputDecoration(
-                      labelText: "Id do dono",
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  hintText: "yyyy-MM-dd",
+                ),
+                onChanged: (value) {
+                  form["date_of_birth"] = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
+                width: 300.0,
+                child: TextField(
+                  controller: weightController,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Peso",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
                       ),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
                     ),
-                    onChanged: (value) {
-                      form["owner_id"] = value;
-                    },
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: ElevatedButton(
-                      onPressed: createPet, child: Text("Cadastrar")))
-            ],
-          ),
-        ));
+                  ),
+                  onChanged: (value) {
+                    form["weight"] = value;
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: ownerIdController,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontSize: 13,
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Id do dono",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                onChanged: (value) {
+                  form["owner_id"] = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: createPet,
+                child: Text("Cadastrar"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/user_create_page.dart
+++ lib/pages/user_create_page.dart
@@ -6,143 +6,194 @@ class UserCreatePage extends StatefulWidget {
   const UserCreatePage({super.key});
 
   @override
-  State<UserCreatePage> createState() => _UserInfoPageState();
+  State<UserCreatePage> createState() => _UserCreatePageState();
 }
 
-class _UserInfoPageState extends State<UserCreatePage> {
+class _UserCreatePageState extends State<UserCreatePage> {
   final form = <String, dynamic>{};
+
+  // Controllers para os campos de texto
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final addressController = TextEditingController();
+  final passwordController = TextEditingController();
 
   void createUser() async {
     final userCreate = UserCreate.fromJson(form);
     await api.postUserUsersPost(userCreate!);
+    form.clear();
+    
+    // Limpar os campos após a criação do usuário
+    nameController.clear();
+    emailController.clear();
+    phoneController.clear();
+    addressController.clear();
+    passwordController.clear();
+    
+    // Exibir popup de confirmação
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Sucesso"),
+          content: Text("Usuário cadastrado com sucesso!"),
+          actions: <Widget>[
+            ElevatedButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar o diálogo
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Cadastrar Usuário"),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        body: Container(
-          alignment: Alignment.topCenter,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: SizedBox(
-                      width: 300.0,
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        maxLines: 1,
-                        style: const TextStyle(
-                          fontSize: 13,
-                        ),
-                        decoration: const InputDecoration(
-                          labelText: "Nome",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          form["name"] = value;
-                        },
-                      ))),
-              SizedBox(
+      appBar: AppBar(
+        title: Text("Cadastrar Usuário"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
                 width: 300.0,
                 child: TextField(
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    style: const TextStyle(
-                      fontSize: 13,
-                    ),
-                    decoration: const InputDecoration(
-                      labelText: "E-mail",
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                  controller: nameController,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Nome",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
                       ),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
                     ),
-                    onChanged: (value) {
-                      form["email"] = value;
-                    }),
+                  ),
+                  onChanged: (value) {
+                    form["name"] = value;
+                  },
+                ),
               ),
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: SizedBox(
-                      width: 300.0,
-                      child: TextField(
-                          textAlign: TextAlign.center,
-                          maxLines: 1,
-                          style: const TextStyle(
-                            fontSize: 13,
-                          ),
-                          decoration: const InputDecoration(
-                            labelText: "Telefone",
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.black,
-                              ),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                            ),
-                          ),
-                          onChanged: (value) {
-                            form["phone"] = value;
-                          }))),
-              SizedBox(
-                  width: 300.0,
-                  child: TextField(
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    style: const TextStyle(
-                      fontSize: 13,
+            ),
+            SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: emailController,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontSize: 13,
+                ),
+                decoration: const InputDecoration(
+                  labelText: "E-mail",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
                     ),
-                    decoration: const InputDecoration(
-                      labelText: "Endereço",
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                onChanged: (value) {
+                  form["email"] = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
+                width: 300.0,
+                child: TextField(
+                  controller: phoneController,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Telefone",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
                       ),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
                     ),
-                    onChanged: (value) {
-                      form["address"] = value;
-                    },
-                  )),
-              Padding(
-                  padding: EdgeInsets.all(20),
-                  child: SizedBox(
-                      width: 300.0,
-                      child: TextField(
-                        textAlign: TextAlign.center,
-                        maxLines: 1,
-                        style: const TextStyle(
-                          fontSize: 13,
-                        ),
-                        decoration: const InputDecoration(
-                          labelText: "Senha",
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black,
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          form["password"] = value;
-                        },
-                      ))),
-              ElevatedButton(onPressed: createUser, child: Text("Cadastrar"))
-            ],
-          ),
-        ));
+                  ),
+                  onChanged: (value) {
+                    form["phone"] = value;
+                  },
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: addressController,
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontSize: 13,
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Endereço",
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                onChanged: (value) {
+                  form["address"] = value;
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: SizedBox(
+                width: 300.0,
+                child: TextField(
+                  controller: passwordController,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  style: const TextStyle(
+                    fontSize: 13,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Senha",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    form["password"] = value;
+                  },
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: createUser,
+              child: Text("Cadastrar"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
